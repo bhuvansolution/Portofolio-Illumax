bc7de9d3f5728c4bb23d75433b2ffb3351dc5e2b
Muhamad Dwiky Finastika Watimena <dwikyfinastika14@gmail.com>
2025-05-18 08:38:41 +0700

    fix frontend

--- public/assets/js/Simple-Slider.js
+++ public/assets/js/Simple-Slider.js
@@ -1,19 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {
-
-    // Initializing the swiper plugin for the slider.
-    // Read more here: http://idangero.us/swiper/api/
-    
+document.addEventListener("DOMContentLoaded", function () {
+    // Initializing the swiper plugin for the slider with autoplay.
     var mySwiper = new Swiper('.swiper-container', {
         loop: true,
+        autoplay: {
+            delay: 3000, // 3 seconds
+            disableOnInteraction: false // Autoplay continues even after interaction
+        },
         pagination: {
-            el: '.swiper-pagination' ,
+            el: '.swiper-pagination',
             clickable: true
         },
-        paginationClickable: true,
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev'
         }
     });
-    
 });
